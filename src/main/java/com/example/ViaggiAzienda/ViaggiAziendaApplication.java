6ce8d7542eadfea168256bfbd36c9bee59874e30
Gabriele Cipolloni <cipolloni204@gmail.com>
2025-02-14 16:45:30 +0100

    first comm

---
+++ src/main/java/com/example/ViaggiAzienda/ViaggiAziendaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ViaggiAzienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViaggiAziendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViaggiAziendaApplication.class, args);
+	}
+
+}
